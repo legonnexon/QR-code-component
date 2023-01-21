ccbd6f777c5ea3a6747e0d7b52579b787a53337c
legonnexon <gildalendario@gmail.com>
2023-01-20 21:09:52 -0300

    modified:   .gitignore 	new file:   design/desktop-design.jpg 	new file:   design/desktop-preview.jpg 	new file:   design/mobile-design.jpg 	modified:   package-lock.json 	modified:   package.json 	new file:   public/favicon-32x32.png 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   public/manifest.json 	new file:   screenshot.jpg 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/images/image-qr-code.png 	deleted:    src/logo.svg 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+import qr_code from './images/image-qr-code.png'
+
+const Title = styled.span`
+font-family: 'Outfit', sans-serif;
+font-size: 22px;
+font-weight: 700;
+color: hsl(218, 44%, 22%);
+margin: 20px;
+`;
+
+const SubText = styled.span`
+font-family: 'Outfit', sans-serif;
+font-size: 15px;
+font-weight: 400;
+color: hsl(220, 15%, 55%);
+margin: 0 25px;
+`;
+
+const Imagen = styled.img`
+margin: 0 auto;
+border-radius: 15px;
+`;
+
+const Figure = styled.figure`
+display: flex;
+flex-direction: column;
+text-align: center;
+margin: 15px auto;
+`;
+
+const Container = styled.div`
+display: flex;
+vertical-align: middle;
+width: 330px;
+height: 500px;
+margin: 0 auto;
+background-color: hsl(0, 0%, 100%);
+border-radius: 25px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Figure>
+        <Imagen 
+          src={qr_code}
+          alt='Qr code for Front End Mentor website'
+          width={'300px'}
+        />
+        <Title>Improve your front-end skills by building projects</Title>
+        <SubText>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</SubText>
+      </Figure>
+      
+    </Container>
   );
 }
 
